56d5d49bcd3dab9ca6df8d06a49aed63178dde78
manish-1498 <manishpradhan14121998@gmail.com>
2021-07-26 17:18:54 +0530

    [manish]:added:Nlog demo

---
+++ NlogProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NlogProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sum sum = new Sum();
+            sum.Addition(11,12);
+        }
+    }
+}
---
+++ NlogProject/Sum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NlogProject
+{
+    class Sum
+    {
+        static Nlog nlog = new Nlog();
+        public int Addition(int firstnumber, int secondnumber)
+        {
+            
+            if (firstnumber == 0 || secondnumber == 0)
+            {
+
+            nlog.LogInfo("Sub method");
+            nlog.LogDebug("Debug Successful");
+            nlog.LogError("First number or second number is zero");
+            return 0;
+            }
+            int result = firstnumber + secondnumber;
+            nlog.LogInfo("Sub method");
+            nlog.LogDebug("Debug Successful");
+            nlog.LogWarn("Warning");
+            return result;
+        }
+    }
+}
